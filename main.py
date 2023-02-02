48e404489c32ec27522dd7c31274082e1bee68bb
Mishan <95490039+MishanGhimire@users.noreply.github.com>
2023-02-02 16:26:24 +0000

    code

--- main.py
+++ main.py
@@ -1,38 +1,104 @@
-# comments
+# # comments
 
-# variables
+# # variables
 
-# print('Mishan Ghimire')
+# # print('Mishan Ghimire')
 
-# name = 'Mishan Ghimire'
-# age = 18
+# # name = 'Mishan Ghimire'
+# # age = 18
 
-# print(name)
-# print(age)
+# # print(name)
+# # print(age)
 
-# full_name = 'Mishan Ghimire'
-# print(full_name)
+# # full_name = 'Mishan Ghimire'
+# # print(full_name)
 
-# width, height = 400, 500
-# print(width)
-# print(height)
+# # width, height = 400, 500
+# # print(width)
+# # print(height)
 
-# your_name = input('Please enter your name: ')
-# print('hi ' + your_name)
+# # your_name = input('Please enter your name: ')
+# # print('hi ' + your_name)
 
-# num1 = input('enter a number: ')
-# num2 = input('enter a number: ')
+# # num1 = input('enter a number: ')
+# # num2 = input('enter a number: ')
 
-# sum = int(num1) + int(num2)
-# # for integers you need to write int()
-# print(sum)
+# # sum = int(num1) + int(num2)
+# # # for integers you need to write int()
+# # print(sum)
 
-# How much do you pay in total for $100 meal with 20% tip?
+# # How much do you pay in total for $100 meal with 20% tip?
 
-food_amount = float(input('enter food amount $: '))
-tip_percentage = float(input('enter your tip percentage %: ')) / 100
-tip_amount = food_amount * tip_percentage
+# # food_amount = float(input('enter food amount $: '))
+# # tip_percentage = float(input('enter your tip percentage %: ')) / 100
+# # tip_amount = food_amount * tip_percentage
 
-#total food amount + tip amount
-total = food_amount + tip_amount
-print('$' + str(total))
+# # #total food amount + tip amount
+# # total = food_amount + tip_amount
+# # print('$' + str(total))
+
+# # String Formatting
+
+# # Boolean
+# # If then Else
+
+# # If withdrawl amount > balance:
+# # don't allow balance
+# # else:
+# #       allow withdrawal
+
+# # baby weather app
+# # if it's raining outside,grab an umbrella
+# # otherwise, grab your sunglasses
+
+# # Boolean ('True','False')
+
+# # weather = input('How is the weather? ')
+
+# # if weather == 'rain':
+# #     print('umbrella')
+# # elif weather == 'cloudy':
+# #     print('cloud')
+# # elif weather == 'thunderstorm':
+# #     print('thunder')
+# # else:
+# #     print('sunglasses')
+# # Comparison operators (==,<=,>=,<,>,)
+
+# # score = 50
+
+# #  >=90 = A
+# # >=80 = B
+# # >=70 = C
+# # >=60 = D
+# # <60 = F
+
+# score = 80
+
+# if score >= 90:
+#   print('A')
+
+# elif score >= 80:
+#   print('B')
+
+# elif score >= 70:
+#   print('C')
+
+# elif score >= 60:
+#   print('D')
+
+# else:
+#   print('F')
+
+# either a passing grade or failing grade
+
+# score = 80
+# if score >= 60 and score <= 100:
+#   print('passing grade')
+
+
+def say_my_name():
+    print('Mishan')
+
+
+say_my_name()
