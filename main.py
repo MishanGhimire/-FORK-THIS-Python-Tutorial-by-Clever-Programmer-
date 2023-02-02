115a4bdac170c212701c3802bbfb92049f8d7fef
Mishan <95490039+MishanGhimire@users.noreply.github.com>
2023-02-02 11:46:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+# comments
+
+# variables
+
+# print('Mishan Ghimire')
+
+# name = 'Mishan Ghimire'
+# age = 18
+
+# print(name)
+# print(age)
+
+# full_name = 'Mishan Ghimire'
+# print(full_name)
+
+# width, height = 400, 500
+# print(width)
+# print(height)
+
+# your_name = input('Please enter your name: ')
+# print('hi ' + your_name)
+
+# num1 = input('enter a number: ')
+# num2 = input('enter a number: ')
+
+# sum = int(num1) + int(num2)
+# # for integers you need to write int()
+# print(sum)
+
+# How much do you pay in total for $100 meal with 20% tip?
+
+food_amount = float(input('enter food amount $: '))
+tip_percentage = float(input('enter your tip percentage %: ')) / 100
+tip_amount = food_amount * tip_percentage
+
+#total food amount + tip amount
+total = food_amount + tip_amount
+print('$' + str(total))
